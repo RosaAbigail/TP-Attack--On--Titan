8b7f5713384968ebda941ec93a3bfdc428287c9d
RosaAbigail <105310834+RosaAbigail@users.noreply.github.com>
2022-05-26 10:26:20 -0300

    Create Suero.java

---
+++ Suero.java
@@ -0,0 +1,38 @@
+package juego;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+import java.awt.*;
+
+public class Suero {
+	// Variables
+	private double x;
+	private double y;
+	private double radio;
+	Image s;
+	
+	// Constructores
+	public Suero(double x, double y) {
+		this.x = x;
+		this.y = y;
+		this.radio = 25;
+		this.s = Herramientas.cargarImagen("s.png"); 
+	}
+	
+	// Metodos
+	public void dibujar(Entorno e) {
+		e.dibujarImagen(this.s, this.x, this.y, 0);
+	}
+	
+  public double getX() {
+		return this.x;
+	}
+	
+  public double getY() {
+		return this.y;
+	}
+	
+  public double getRadio() {
+		return this.radio;
+	}
+}

bbda241f9fa4cafc72b21da801fcbf4a5053ae4d
RosaAbigail <105310834+RosaAbigail@users.noreply.github.com>
2022-05-28 18:11:45 -0300

    Update Obstaculo.java

--- Obstaculo.java
+++ Obstaculo.java
@@ -20,33 +20,33 @@ public class Obstaculo {
 	}
 	
 	// Metodos
-	public void dibujar(Entorno e) {
-		e.dibujarImagen(this.o, this.x, this.y, 0);
-	}
-	
-  public static void crearObstaculos(Entorno e, Obstaculo[] o) {
+	public static void crearObstaculos(Entorno e, Obstaculo[] o) {
 	    for (int i = 0; i < o.length; i++) {
 	    	double x = 0;
 			double y = 0;
 			o[i] = new Obstaculo(x, y);
 		}
-		o[0] =  new Obstaculo(650, 525);
-		o[1] =  new Obstaculo(400, 260);
-		o[2] =  new Obstaculo(900, 260);
-		o[3] =  new Obstaculo(1175, 200);
-		o[4] =  new Obstaculo(150, 600);
-		o[5] =  new Obstaculo(1150, 600);
+		o[0] =  new Obstaculo(100, 100);
+		o[1] =  new Obstaculo(100, 400);
+		o[2] =  new Obstaculo(400, 625);
+		o[3] =  new Obstaculo(500, 100);
+		o[4] =  new Obstaculo(800, 550);
+		o[5] =  new Obstaculo(1150, 350);
 	}
 	
-  public double getX() {
+	public void dibujar(Entorno e) {
+		e.dibujarImagen(this.o, this.x, this.y, 0);
+	}
+	
+	public double getX() {
 		return this.x;
 	}
 	
-  public double getY() {
+	public double getY() {
 		return this.y;
 	}
-	
-  public double getRadio() {
+
+	public double getRadio() {
 		return this.radio;
 	}
 }
